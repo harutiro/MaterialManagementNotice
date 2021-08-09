6b572dba085742c0543bd4514258ab021742ee4b
harutiro <harutiro2027@outlook.jp>
2021-08-09 12:59:40 +0900

    add: 各アクティビティの作成

---
+++ app/src/main/java/app/makino/harutiro/materialmanagementnotice/EditTagActivity.kt
@@ -0,0 +1,11 @@
+package app.makino.harutiro.materialmanagementnotice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditTagActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_tag)
+    }
+}
